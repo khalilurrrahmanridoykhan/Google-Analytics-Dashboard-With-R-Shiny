d856a7c76866e88aae5c6e23141acd9bd19bcc27
khalilurrrahmanridoykhan <khalilurrahmanridoykhan@gmail.com>
2024-10-01 03:27:12 +0600

    Dinamic Visualization

--- app.R
+++ app.R
@@ -1,102 +1,107 @@
 library(shiny)
 library(tidyverse)
 library(plotly)
-library(here)
+library(purrr)
+
+
+# source_all_files <- function(directory) {
+#   files <- list.files(directory, full.names = TRUE, pattern = "\\.[Rr]$")
+#   for (file in files) {
+#     tryCatch({
+#       message("Sourcing file: ", file)
+#       source(file)
+#     }, error = function(e) {
+#       message("Error sourcing file: ", file)
+#       message("Error message: ", e$message)
+#     })
+#   }
+# }
+# source_all_files(here("./google_analytics_viz.R"))
+source("google_analytics_viz.R")
+
+list.files(here::here("R")) %>%
+    here::here("R", .) %>%
+    purrr::walk(~source(.))
 
 ui <- fluidPage(
-  br(),
-  div(
-    class = "class-a",
-    div(
-      class = "col-md-6 col-lg-6 col-sm-12",
-      div(
-        class = "panel panel-default",
-        div(
-          class = "panel-heading clearfix",
-          tags$h2("Viz 1", class = "pull-left panel-title"),
-          div(
-            class = "pull-right",
-            shiny::actionButton(
-              inputId = "a",
-              label = "",
-              class = "btn-danger delete",
-              icon = shiny::icon("minus")
-            )
-          )
-        ),
-        div(
-          class = "panel-body",
-          plotly::plot_ly(
-            mtcars,
-            x = ~mpg, y = ~hp, type = "scatter", mode = "markers"
-          )
-        )
-      )
-    )
-  ),
-  div(
-    class = "class-b",
-    div(
-      class = "col-md-6 col-lg-6 col-sm-12",
-      div(
-        class = "panel panel-default",
-        div(
-          class = "panel-heading clearfix",
-          tags$h2("Viz 2", class = "pull-left panel-title"),
-          div(
-            class = "pull-right",
-            shiny::actionButton(
-              inputId = "b",
-              label = "",
-              class = "btn-danger delete",
-              icon = shiny::icon("minus")
-            )
-          )
-        ),
-        div(
-          class = "panel-body",
-          plotly::plot_ly(
-            mtcars,
-            x = ~mpg, y = ~hp, type = "scatter", mode = "markers"
-          )
-        )
-      )
-    )
-  ),
-  div(
-    class = "class-c",
-    div(
-      class = "col-md-6 col-lg-6 col-sm-12",
-      div(
-        class = "panel panel-default",
-        div(
-          class = "panel-heading clearfix",
-          tags$h2("Viz 3", class = "pull-left panel-title"),
-          div(
-            class = "pull-right",
-            shiny::actionButton(
-              inputId = "c",
-              label = "",
-              class = "btn-danger delete",
-              icon = shiny::icon("minus")
-            )
-          )
-        ),
-        div(
-          class = "panel-body",
-          plotly::plot_ly(
-            mtcars,
-            x = ~mpg, y = ~wt, type = "scatter", mode = "markers"
-          )
-        )
-      )
-    )
-  ),
-  shiny::includeScript(here::here("./scripts.js"))
-)
 
+    br(),
+    br(),
+    div(id = "placeholder"),
+    shiny::tagList(
+
+        shiny::uiOutput(outputId = "first_plots")
+
+    ),
+
+    shiny::includeScript(here::here("./scripts.js"))
+
+) 
+
+
+
+
+
+# Define server logic required to draw a histogram
 server <- function(input, output) {
+
+
+    output$first_plots <- shiny::renderUI({
+
+        purrr::pmap(
+            list(
+                x = c("a", "b", "c"),
+                y = c("Viz 1", "Viz 2", "Viz 3")
+            ),
+                    function(x, y) {
+            google_analytics_viz(
+                title = y,
+                viz = NULL,
+                df = NULL,
+                btn_id = x,
+                class_all = "delete",
+                class_specific = paste0("class_", x),
+                color = "danger"
+            )
+        }
+        )
+
+    })
+
+    # run when we add visualization
+    shiny::observeEvent(input$add_btn_clicked, {
+
+        # clicked id
+        panel <- input$add_btn_clicked
+
+        panel_plot_item <-
+            if (exists("google_analytics_viz")) {
+                google_analytics_viz(
+                    title = input$header,
+                    viz = NULL,
+                    df = NULL,
+                    btn_id = panel,
+                    class_all = "delete",
+                    class_specific = paste0("class_", panel),
+                    color = "danger"
+                )
+            } else {
+                print("Function google_analytics_viz is not defined.")
+            }
+
+        css_selector <- ifelse(input$last_panel == "#placeholder",
+                               "#placeholder",
+                               paste0(".", input$last_panel)
+        )
+
+        shiny::insertUI(
+            selector = css_selector,
+            "afterEnd",
+            ui = panel_plot_item
+        )
+    })
 
 }
 
+# Run the application
 shinyApp(ui = ui, server = server)
--- scripts.js
+++ scripts.js
@@ -2,21 +2,48 @@ $(document).on('click', ".delete", function(){
     click_id = $(this).attr('id');
     header_h2 = $("#" + click_id).parent().parent().text().trim();
     // console.log(header_h2);
-    $(".class-"+ click_id).remove();
+    $(".class_"+ click_id).remove();
 
     html = `
-    <div class="added-`+ click_id +`"><a id="`+ click_id +`" href="#" class="action-button">` + header_h2 + `</a></div>
+    <div class="added_`+ click_id +`"><a id="`+ click_id +`" href="#" class="action-button added_btn">` + header_h2 + `</a></div>
     `;
-    console.log(html);
-    if($('[class^="added-"]').length){
-        last_class_added = document.querySelectorAll('[class^="added-"]');
+    // console.log(html);
+    if($('[class^="added_"]').length){
+        last_class_added = document.querySelectorAll('[class^="added_"]');
         last_added = last_class_added[last_class_added.length -1].className;
         $(html).insertAfter("."+ last_added);
     }else{
-        last_class_added = document.querySelectorAll('[class^="class-"]');
+        last_class_added = document.querySelectorAll('[class^="class_"]');
         last_added = last_class_added[last_class_added.length -1].className;
         $(html).insertAfter("."+ last_added);
     }
+});
+
+$(document).on("click", ".added_btn", function() {
+
+    var clicked_id = $(this).attr('id');
+    var p = $("#" + clicked_id).parent().text();
+    var p = $.trim(p);
+    Shiny.setInputValue('header', p, {priority: 'event'});
+    $(".added_" + clicked_id).remove();
+  
+    if($("[class^='class_']").length) {
+        last_panel = $("[class^='class_']").last().attr("class");
+        Shiny.setInputValue('last_panel', last_panel, {priority: 'event'});
+    } else {
+        Shiny.setInputValue('last_panel', '#placeholder', {priority: 'event'});
+    }
+  
+    Shiny.setInputValue('add_btn_clicked', clicked_id, {priority: 'event'});
+  
+  });
+
+  $(document).ready(function() {
+    // Suppress error alerts
+    window.onerror = function(message, source, lineno, colno, error) {
+        console.log("Error: ", message);
+        return true; // Prevents the firing of the default event handler
+    };
 });
 
 // alert("I am working");
